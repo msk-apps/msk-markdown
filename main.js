7e598c93e773d32dcbed5bc2ba1fb90216cd5f8d
KhantParthkumarA <parthkhant1@gmail.com>
2021-10-02 23:55:57 +0530

    Add window event

--- main.js
+++ main.js
@@ -328,6 +328,15 @@ CloseAbout.addEventListener("click", OpenCloseAbout);
 ToggleSettings.addEventListener("click", OpenCloseSettings);
 CloseSettings.addEventListener("click", OpenCloseSettings);
 
+window.onclick = ({target}) => {
+    if (target == Settings) {
+        OpenCloseSettings();
+    }
+    if (target == About) {
+        OpenCloseAbout();
+    }
+};
+
 function DownloadFile() {
     let DownloadName = document.getElementById("SaveFileInput").value;
     let text = document.getElementById("getm").value;
